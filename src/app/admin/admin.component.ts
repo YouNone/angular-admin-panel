973027076c74bae8f07ed815b54a418e164e4d29
developer01 <developer01@academydev.loc>
2020-02-25 18:01:25 +0200

    styling\ sidenav

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -9,14 +9,14 @@ import { MediaMatcher } from '@angular/cdk/layout';
 })
 export class AdminComponent {
 	/** По умолчанию mat-sidenav открыт */
-	private _mobileQueryListener: () => void;
-	public sideNavToggle = true;
+	// private _mobileQueryListener: () => void;
 	mobileQuery: MediaQueryList;
+	public sideNavToggle = true;
 
 	constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) { 
 		this.mobileQuery = media.matchMedia('(max-width: 600px)');
-		this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-		this.mobileQuery.addListener(this._mobileQueryListener);
+		// this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+		// this.mobileQuery.addListener(this._mobileQueryListener);
 	}
 
 	stateToggle(barState: boolean) {
